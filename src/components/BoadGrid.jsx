b3af893b03d6e0c9531ec06c4f959722e1f3534b
masanorimiyamoto-cloud <mikekoro777@gmail.com>
2025-03-20 20:49:55 +0900

    Boadtest

--- src/components/BoadGrid.jsx
+++ src/components/BoadGrid.jsx
@@ -1,39 +1,10 @@
 // src/components/BoardGrid.jsx
 import React from "react";
-import Cell from "./Cell_com";
-import "../styles/BoardGrid.css"; // 必要なら CSS を別途用意
 
-export default function BoardGrid({
-  board,
-  problemCells,
-  errorCells,
-  selectedCell,
-  onCellClick,
-}) {
+export default function BoardGrid(props) {
   return (
     <div className="board-grid">
-      {board.map((rowArr, r) =>
-        rowArr.map((val, c) => {
-          const isFixed = problemCells.some(
-            ([pr, pc]) => pr === r && pc === c
-          );
-          const isError = errorCells.some(
-            ([er, ec]) => er === r && ec === c
-          );
-          const isSelected =
-            selectedCell?.row === r && selectedCell?.col === c;
-          return (
-            <Cell
-              key={`${r}-${c}`}
-              value={val}
-              isFixed={isFixed}
-              isError={isError}
-              isSelected={isSelected}
-              onClick={() => onCellClick(r, c)}
-            />
-          );
-        })
-      )}
+      <p>BoardGrid works!</p>
     </div>
   );
 }
